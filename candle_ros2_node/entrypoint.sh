e6d006423c06945ab846548623216e770518bfa0
Błażej Szargut <blazejszargut@gmail.com>
2025-09-02 23:24:26 +0000

    Add Docker configuration and entrypoint for bernard_ros2_pkgs, update docker-compose and wait-for-candle script

--- candle_ros2_node/entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Run node
-source /candle_ros2_ws/install/setup.bash
-exec ros2 run candle_ros2 candle_ros2_node USB 8M
--- micro_ros_agent/wait-for-candle.sh
+++ micro_ros_agent/wait-for-candle.sh
@@ -2,8 +2,8 @@
 
 source /opt/ros/${ROS_DISTRO}/setup.bash
 
-until ros2 node list | grep -q candle_ros2_node; do
-  echo "Waiting for candle_ros2_node to be available..."
+until ros2 node list | grep -q actuator_state_publisher; do
+  echo "Waiting for actuator_state_publisher to be available..."
   sleep 2
 done
 exec "$@"
