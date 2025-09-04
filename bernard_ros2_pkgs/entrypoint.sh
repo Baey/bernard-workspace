9f8b616cf035245a6b4befc3215bf6966fd5e47b
Błażej Szargut <blazejszargut@gmail.com>
2025-09-04 13:11:43 +0000

    Add .gitmodules for bernard_hw_if and bernard_state_publisher, remove Dockerfile and entrypoint.sh, and update docker-compose.yml

--- bernard_ros2_pkgs/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-source /opt/ros/jazzy/setup.bash
-source /bernard_ws/install/setup.bash
-
-ros2 run bernard_hw_if actuator_state_publisher &
-
-ros2 launch bernard_state_publisher state.launch.py
